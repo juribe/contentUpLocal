102ce030e6f7356c8ad4a79b4b1fb6a74a6b945d
joseuribe <jse.uribe@outlook.com>
2022-03-11 09:12:53 -0500

    remove code example

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -3,10 +3,4 @@ class Publisher < ApplicationRecord
     has_rich_text :header
     has_rich_text :footer
     has_many :publisher_contents
-    after_save :reload_routes
-    def reload_routes
-        if self.name_changed?
-            Redis.send("rails_routes_ts", "expired")
-        end
-    end
 end
